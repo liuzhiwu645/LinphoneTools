70f033f324c4e2efe5788daaaf1ae54b22a0c9f6
910300041@qq.com <yeqiaojituan@yeqiaojituandeiMac.local>
2019-10-29 15:48:34 +0800

    LinphoneTool

---
+++ LinphoneTool/LinphoneManager.h
@@ -0,0 +1,195 @@
+
+#import <Foundation/Foundation.h>
+#import <SystemConfiguration/SCNetworkReachability.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AssetsLibrary/ALAssetsLibrary.h>
+#import <CoreTelephony/CTCallCenter.h>
+#import <sqlite3.h>
+#include "linphone/linphonecore.h"
+#include "bctoolbox/list.h"
+#import <UIKit/UIKit.h>
+#define LINPHONE_LOGS_MAX_ENTRY 5000
+#define IPAD (LinphoneManager.runningOnIpad)
+#define LC ([LinphoneManager getLc])
+
+extern NSString *const LINPHONERC_APPLICATION_KEY;
+
+extern NSString *const kLinphoneCoreUpdate;
+extern NSString *const kLinphoneDisplayStatusUpdate;
+extern NSString *const kLinphoneMessageReceived;
+extern NSString *const kLinphoneTextComposeEvent;
+extern NSString *const kLinphoneCallUpdate;
+extern NSString *const kLinphoneRegistrationUpdate;
+extern NSString *const kLinphoneMainViewChange;
+extern NSString *const kLinphoneAddressBookUpdate;
+extern NSString *const kLinphoneLogsUpdate;
+extern NSString *const kLinphoneSettingsUpdate;
+extern NSString *const kLinphoneBluetoothAvailabilityUpdate;
+extern NSString *const kLinphoneConfiguringStateUpdate;
+extern NSString *const kLinphoneGlobalStateUpdate;
+extern NSString *const kLinphoneNotifyReceived;
+extern NSString *const kLinphoneNotifyPresenceReceivedForUriOrTel;
+extern NSString *const kLinphoneCallEncryptionChanged;
+extern NSString *const kLinphoneFileTransferSendUpdate;
+extern NSString *const kLinphoneFileTransferRecvUpdate;
+
+typedef enum _NetworkType {
+    network_none = 0,
+    network_2g,
+    network_3g,
+    network_4g,
+    network_lte,
+    network_wifi
+} NetworkType;
+
+typedef enum _Connectivity {
+	wifi,
+	wwan,
+    none
+} Connectivity;
+
+extern const int kLinphoneAudioVbrCodecDefaultBitrate;
+
+/* Application specific call context */
+typedef struct _CallContext {
+    LinphoneCall* call;
+    bool_t cameraIsEnabled;
+} CallContext;
+
+struct NetworkReachabilityContext {
+    bool_t testWifi, testWWan;
+    void (*networkStateChanged) (Connectivity newConnectivity);
+};
+
+@interface LinphoneCallAppData :NSObject {
+    @public
+	bool_t batteryWarningShown;
+    UILocalNotification *notification;
+    NSMutableDictionary *userInfos;
+	bool_t videoRequested; /*set when user has requested for video*/
+    NSTimer* timer;
+};
+@end
+
+typedef struct _LinphoneManagerSounds {
+    SystemSoundID vibrate;
+} LinphoneManagerSounds;
+
+@interface LinphoneManager : NSObject {
+@protected
+	SCNetworkReachabilityRef proxyReachability;
+
+@private
+	NSTimer* mIterateTimer;
+    NSMutableArray*  pushCallIDs;
+	Connectivity connectivity;
+    UIBackgroundTaskIdentifier pausedCallBgTask;
+    UIBackgroundTaskIdentifier incallBgTask;
+    UIBackgroundTaskIdentifier pushBgTask;
+	CTCallCenter* mCallCenter;
+    NSDate *mLastKeepAliveDate;
+@public
+    CallContext currentCallContextBeforeGoingBackground;
+}
++ (LinphoneManager*)instance;
+
++ (LinphoneCore*) getLc;
++ (BOOL)runningOnIpad;
++ (NSString *)getPreferenceForCodec: (const char*) name withRate: (int) rate;
++ (BOOL)isCodecSupported: (const char*)codecName;
++ (NSSet *)unsupportedCodecs;
++ (int)unreadMessageCount;
+
+
+- (void)resetLinphoneCore;
+- (void)startLinphoneCore;
+- (void)destroyLinphoneCore;
+- (BOOL)resignActive;
+- (void)becomeActive;
+- (BOOL)enterBackgroundMode;
+- (void)addPushCallId:(NSString*) callid;
+- (void)configurePushTokenForProxyConfig: (LinphoneProxyConfig*)cfg;
+- (BOOL)popPushCallID:(NSString*) callId;
+- (void)acceptCallForCallId:(NSString*)callid;
+- (LinphoneCall *)callByCallId:(NSString *)call_id;
+- (void)cancelLocalNotifTimerForCallId:(NSString*)callid;
+- (void)startPushLongRunningTask:(BOOL)msg;
++ (void)kickOffNetworkConnection;
+- (void)setupNetworkReachabilityCallback;
+
+- (void)refreshRegisters;
+
+- (bool)allowSpeaker;
+
+- (void)configureVbrCodecs;
+
++ (BOOL)copyFile:(NSString*)src destination:(NSString*)dst override:(BOOL)override;
++ (NSString*)bundleFile:(NSString*)file;
++ (NSString*)documentFile:(NSString*)file;
++ (NSString*)cacheDirectory;
+
+- (void)acceptCall:(LinphoneCall *)call evenWithVideo:(BOOL)video;
+- (void)call:(const LinphoneAddress *)address;
+- (BOOL)doCall:(const LinphoneAddress *)iaddr;
+
++(id)getMessageAppDataForKey:(NSString*)key inMessage:(LinphoneChatMessage*)msg;
++(void)setValueInMessageAppData:(id)value forKey:(NSString*)key inMessage:(LinphoneChatMessage*)msg;
+
+- (void)lpConfigSetString:(NSString*)value forKey:(NSString*)key;
+- (void)lpConfigSetString:(NSString *)value forKey:(NSString *)key inSection:(NSString *)section;
+- (NSString *)lpConfigStringForKey:(NSString *)key;
+- (NSString *)lpConfigStringForKey:(NSString *)key inSection:(NSString *)section;
+- (NSString *)lpConfigStringForKey:(NSString *)key withDefault:(NSString *)value;
+- (NSString *)lpConfigStringForKey:(NSString *)key inSection:(NSString *)section withDefault:(NSString *)value;
+
+- (void)lpConfigSetInt:(int)value forKey:(NSString *)key;
+- (void)lpConfigSetInt:(int)value forKey:(NSString *)key inSection:(NSString *)section;
+- (int)lpConfigIntForKey:(NSString *)key;
+- (int)lpConfigIntForKey:(NSString *)key inSection:(NSString *)section;
+- (int)lpConfigIntForKey:(NSString *)key withDefault:(int)value;
+- (int)lpConfigIntForKey:(NSString *)key inSection:(NSString *)section withDefault:(int)value;
+
+- (void)lpConfigSetBool:(BOOL)value forKey:(NSString*)key;
+- (void)lpConfigSetBool:(BOOL)value forKey:(NSString *)key inSection:(NSString *)section;
+- (BOOL)lpConfigBoolForKey:(NSString *)key;
+- (BOOL)lpConfigBoolForKey:(NSString *)key inSection:(NSString *)section;
+- (BOOL)lpConfigBoolForKey:(NSString *)key withDefault:(BOOL)value;
+- (BOOL)lpConfigBoolForKey:(NSString *)key inSection:(NSString *)section withDefault:(BOOL)value;
+
+- (void)silentPushFailed:(NSTimer*)timer;
+
+- (void)removeAllAccounts;
+
++ (BOOL)isMyself:(const LinphoneAddress *)addr;
+- (LinphoneAddress *)normalizeSipOrPhoneAddress:(NSString *)addr;
+
+
+//- (void)setProviderDelegate:(ProviderDelegate *)del;
+
+@property (readonly) BOOL isTesting;
+//@property(readonly, strong) FastAddressBook *fastAddressBook;
+@property Connectivity connectivity;
+@property (readonly) NetworkType network;
+@property (readonly) const char*  frontCamId;
+@property (readonly) const char*  backCamId;
+@property(strong, nonatomic) NSString *SSID;
+@property (readonly) sqlite3* database;
+@property(nonatomic, strong) NSData *pushNotificationToken;
+@property (readonly) LinphoneManagerSounds sounds;
+@property (readonly) NSMutableArray *logs;
+@property (nonatomic, assign) BOOL speakerEnabled;
+@property (nonatomic, assign) BOOL bluetoothAvailable;
+@property (nonatomic, assign) BOOL bluetoothEnabled;
+@property (readonly) ALAssetsLibrary *photoLibrary;
+@property (readonly) NSString* contactSipField;
+@property (readonly,copy) NSString* contactFilter;
+@property (copy) void (^silentPushCompletion)(UIBackgroundFetchResult);
+@property (readonly) BOOL wasRemoteProvisioned;
+@property (readonly) LpConfig *configDb;
+
+@property(strong, nonatomic) NSMutableArray *fileTransferDelegates;
+@property BOOL nextCallIsTransfer;
+@property BOOL conf;
+@property NSDictionary *pushDict;
+
+@end
